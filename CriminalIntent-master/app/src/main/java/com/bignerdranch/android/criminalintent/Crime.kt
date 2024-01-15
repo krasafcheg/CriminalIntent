90f3f6791012cf38b8a1c26c52f2e14663f28605
krasafcheg <153277288+krasafcheg@users.noreply.github.com>
2024-01-15 22:46:32 +0700

    Add files via upload

---
+++ CriminalIntent-master/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+import java.util.UUID
+@Entity
+data class Crime(
+    @PrimaryKey
+    var id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var date: Date = Date(),
+    var isSolved: Boolean = false
+)
